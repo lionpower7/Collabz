0ff5c42c078a9f48159238c16cae8b77f449a0b6
Jeremy B. Frias <jeremyfd77@gmail.com>
2019-01-30 13:33:45 -0500

    Now Run's with PHP

---
+++ Co-PHP/Land.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Collabz | Meet With Enthusiastic Individuals!</title>
+</head>
+<body>
+  <?php include "../templates/index.html"; ?>
+  <style>
+    <?php include "../static/css/index.css"; ?>
+  </style>
+</body>
+</html>
